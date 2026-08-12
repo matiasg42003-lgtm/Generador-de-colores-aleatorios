c225eeeb0f7669c9f293326603fc2709f3f2e213
Matias <matiasg.42003@gmail.com>
2026-08-12 13:58:06 -0300

    Captar seleccion de ususario JS y correciones varias

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log("Funcionando..");
+
+const seleccion = document.getElementById("selectColor");
+const contenedor = document.getElementById("contenedorColores");
+
+
+const generarPaleta = () => {
+
+    seleccion.addEventListener("change", () => {
+
+        let cantidadSeleccionada = seleccion.value;
+        console.log("El usuario eligio: " + cantidadSeleccionada);
+    })
+};
+
+generarPaleta();
+
+
+const generarColorHSL = () => {
+
+    let tonoAlAzar = Math.floor(Math.random() * 361);
+
+    let colorFinal = `hsl(${tonoAlAzar}, 80%, 50%)`;
+    return colorFinal;
+
+}
+
